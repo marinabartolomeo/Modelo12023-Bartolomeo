c8cc6e6e7e567832fd9a73f85a5ee684d169af52
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-03-31 20:34:13 -0300

    Add files via upload

---
+++ ejercicio2-variasvariables.java
@@ -0,0 +1,30 @@
+public class ejercicio2-variasvariables {
+//declaro metodo main
+	public static vold main(String arg[]){
+//declaro variables
+	int NumA =2;
+	int NumB =1;
+	int NumC =5;
+	int NumD =3;
+
+//Mostrar en pantalla las variables
+	System.out.println("------Las Variables------");
+	System.out.println("--El valor de NumA es:" +NumA);
+	System.out.println("--El valor de NumB es:" +NumB);
+	System.out.println("--El valor de NumC es:" +NumC);
+	System.out.println("--El valor de NumD es:" +NumD);
+	
+// las letras toman valores diferentes
+
+NumB = NumC;
+	System.out.println("--El valor de NumB es ="  +NumC);
+NumC = NumA;
+	System.out.println("--El valor de NumC es ="  +NumA); 
+NumA = NumD; 
+	System.out.println("--El valor de NumA es ="  +NumD);
+NumD = NumC;  
+	System.out.println("--El valor de NumD es ="  +NumC);
+
+
+}
+}
