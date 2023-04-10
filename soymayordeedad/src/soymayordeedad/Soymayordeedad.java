60212ae83c4421c2e802d63df9796d715217450a
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-04-10 20:38:28 -0300

    Add files via upload

---
+++ soymayordeedad/src/soymayordeedad/Soymayordeedad.java
@@ -0,0 +1,29 @@
+package soymayordeedad;
+
+/**
+ *
+ * @author Marina
+ */
+public class Soymayordeedad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Variables
+        int edad= 19;
+        
+        //inicio programa
+        System.out.println("----Bienvnidos al Bar MOON-----");
+        System.out.println("---Antes de ingresar necesitamos saber su edad---");
+        System.out.println("Su edad es de " + edad);
+        if (edad > 18){
+        System.out.println("---Tome mas pero no conduzca---");
+        }
+        else {
+        System.out.println("---no puede beber alcohol todavia, tome una Coca COla---");
+        }
+        System.out.println ("-------");
+    }
+    }
+    
