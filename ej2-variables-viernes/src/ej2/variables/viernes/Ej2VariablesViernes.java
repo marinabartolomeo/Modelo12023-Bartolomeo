588bc7ba2d11f3cbda004b8090535ee062b9d34b
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-04-13 18:49:06 -0300

    Add files via upload

---
+++ ej2-variables-viernes/src/ej2/variables/viernes/Ej2VariablesViernes.java
@@ -0,0 +1,28 @@
+package ej2.variables.viernes;
+
+/**
+ *
+ * @author Marina
+ */
+public class Ej2VariablesViernes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      //declaro variables
+	int NumA =50;
+	int NumB =35;
+	int Resultado = NumA + NumB;
+//Mostrar en pantalla las variables
+	System.out.println("------Las Variables------");
+	System.out.println("--El valor de NumA es:" + NumA);
+	System.out.println("--El valor de NumB es:" + NumB);
+	System.out.println("--El valor de Resultado es:" + Resultado);
+	System.out.println("---------------------------------");
+  
+    }
+    
+}
