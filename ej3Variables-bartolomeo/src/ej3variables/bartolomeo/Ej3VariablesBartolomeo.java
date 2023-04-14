ae5de337c91670c28605081fbe48285eeb2a202f
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-04-13 23:16:20 -0300

    Add files via upload

---
+++ ej3Variables-bartolomeo/src/ej3variables/bartolomeo/Ej3VariablesBartolomeo.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej3variables.bartolomeo;
+
+/**
+ *
+ * @author Marina
+ */
+public class Ej3VariablesBartolomeo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Variables
+int numero1;
+int numero2;
+int suma;
+int edad;
+String nombre = "Marina";
+// Constante
+final int NUMERO_3= 4;
+// Ingreso los valores a la variables numero1 y numero2
+//numero1 = 30;
+//numero2 = 40;
+// operación suma
+//suma= numero1 + numero2;
+// Muestro en pantalla las dos variables y el resultado de la suma
+//System.out.println("---El valor de numero1:" +numero1);
+//System.out.println("---El valor de numero2:" +numero2);
+//System.out.println("---El resultado de la suma es:" + suma);
+
+// Cambio los valores de numero1,numero2 y vuelvo a realizar la operación de suma
+numero1 = 100;
+numero2 = 250;
+edad = 47;
+//operación suma
+suma= numero1 + numero2;
+// Muestro en pantalla las dos variables y el resultado de la suma
+System.out.println("---El resultado de la suma es:" + suma);
+
+// Instancio y muestro en pantalla la variable nombre y su edad
+System.out.println("---El nombre es:" + nombre);
+System.out.println("---La edad es:" + edad);
+// Muestro en pantalla la constante
+System.out.println("--- La constante es:" + NUMERO_3);
+
+}
+
+}
+    
+
