d0ab9e5bb3baf35641e2ecdf8a385e5092bf7649
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-04-16 19:34:22 -0300

    Add files via upload

---
+++ ej5Variables-Bartolomeo/src/ej5variables/bartolomeo/Ej5VariablesBartolomeo.java
@@ -0,0 +1,172 @@
+package ej5variables.bartolomeo;
+
+/**
+ *
+ * @author Marina
+ */
+public class Ej5VariablesBartolomeo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declaro Variables
+        int X;
+        int Y;
+        double N;
+        double M;
+        double sumar;
+        double restar;
+        double producto;
+        double producto1;
+        double cociente;
+        double resto;
+        
+        
+        // Le asigno un valor
+        
+        X = 12;
+        Y = 6;
+        N = 2.5;
+        M = 15.5;
+        
+        //Muestro en pantalla
+        
+        System.out.println("----El valor de X es:" + X);
+        System.out.println("----El valor de Y es:" + Y);
+        System.out.println("----El valor de N es:" + N);
+        System.out.println("----El valor de M es:" + M);
+
+    //La suma X + Y
+        //Suma
+         sumar= X + Y;
+            System.out.println("---La suma de X + Y:" + sumar);
+        
+            
+    //La diferencia X – Y   
+    restar= X - Y;
+            System.out.println("---La resta de X - Y:" + restar);
+        
+    //El producto X * Y
+    producto= X * Y;
+            System.out.println("---El producto de X * Y:" + producto);
+    
+    //El cociente X / Y
+    cociente= X / Y;
+            System.out.println("---El cociente de X / Y:" + cociente);
+    
+    //El resto X % Y
+    resto= X % Y;
+            System.out.println("---El resto de X % Y:" + resto);
+
+    //La suma N + M
+    sumar= N + M;
+            System.out.println("---La suma de N + M:" + sumar);
+            
+    //La diferencia N – M
+    restar= N - M;
+            System.out.println("---La suma de N - M:" + restar);
+
+    //El producto N * M
+    producto= N * M;
+            System.out.println("---El producto de N * M:" + producto);
+
+    //El cociente N / M
+    cociente= N / M;
+            System.out.println("---El cociente de N / M:" + cociente);
+
+    //El resto N % M
+    resto= N % M;
+            System.out.println("---El resto de N % M:" + resto);
+    
+    //La suma X + N
+    sumar= X + N;
+            System.out.println("---La suma de X + N:" + sumar);
+
+    //El cociente Y / M
+    cociente= Y / M;
+            System.out.println("---El cociente de Y / M:" + cociente);
+    
+    //El resto Y % M
+    resto= Y % M;
+            System.out.println("---El resto de Y % M:" + resto);
+    
+    //El doble de cada variable
+    producto1= X * 2;
+            System.out.println("---El doble de X es:" + producto1);
+    producto1= Y * 2;
+            System.out.println("---El doble de Y es:" + producto1);
+    producto1= N * 2;
+            System.out.println("---El doble de N es:" + producto1);
+    producto1= M * 2;
+            System.out.println("---El doble de M es:" + producto1);
+            
+    //La suma de todas las variables
+    sumar= X + Y + N + M;
+            System.out.println("---La suma de todas las variables es:" + sumar);
+        
+    //El producto de todas las variables
+    producto= X * Y * N * M;
+            System.out.println("---El producto de de todas las variables es:" + producto);        
+            
+    }
+    
+}
+
+
+
+//Escribe un programa Java que realice lo siguiente: declarar dos variables
+//X e Y de tipo int, dos variables N y M de tipo double y asigna a cada una 
+//un valor. A continuación, muestra por pantalla:
+//El valor de cada variable.
+//La suma X + Y
+//La diferencia X – Y
+//El producto X * Y
+//El cociente X / Y
+//El resto X % Y
+//La suma N + M
+//La diferencia N – M
+//El producto N * M
+//El cociente N / M
+//El resto N % M
+//La suma X + N
+//El cociente Y / M
+//El resto Y % M
+//El doble de cada variable
+//La suma de todas las variables
+//El producto de todas las variables
+//Si por ejemplo le hemos dado a X el valor 1, a Y el valor 2, a M el valor 
+//3.2 y a N el valor 4.7 se debe mostrar por pantalla:
+//Variable X = 1
+//Variable Y = 2
+//Variable M = 3.2
+//Variable N = 4.7
+//1 + 2 = 3
+//1 - 2 = -1
+//1 * 2 = 2
+//1 / 2 = 0
+//1 % 2 = 1
+//4.7 + 3.2 = 7.9
+//4.7 - 3.2 = 1.5
+//4.7 * 3.2 = 15.040000000000001
+//4.7 / 3.2 = 1.46875
+//4.7 % 3.2 = 1.5
+//1 + 4.7 = 5.7
+//2 / 3.2 = 0.625
+//2 % 3.2 = 2.0
+//Variable X = 1 el doble es 2
+//Variable Y = 2 el doble es 4
+//Variable M = 3.2 el doble es 6.4
+//Variable N = 4.7 el doble es 9.4
+//1 + 2 + 4.7 + 3.2 = 10.9
+//1 * 2 * 4.7 * 3.2 = 30.080000000000002
+
+
+//Se necesita :
+//1_Diagrama de flujo 
+//2_Decodificacion (Crear proyecto con el nombre del ejercicio)
+//3_Copilacion
+
+//Nota :Crear una carpeta en repositorio con el nombre del ejercicio y subir 
+//los archivos detallados necesarios. Luego adjuntar a en la resolución 
+//del ejercicio.
