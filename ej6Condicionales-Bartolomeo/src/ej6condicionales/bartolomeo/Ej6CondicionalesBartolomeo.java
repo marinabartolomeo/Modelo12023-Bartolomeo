58d449591617af9e1aa6fcbb1ac1c13b7e827bc4
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-04-25 19:53:29 -0300

    Add files via upload

---
+++ ej6Condicionales-Bartolomeo/src/ej6condicionales/bartolomeo/Ej6CondicionalesBartolomeo.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej6condicionales.bartolomeo;
+
+/**
+ *
+ * @author Marina
+ */
+public class Ej6CondicionalesBartolomeo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Variables
+        
+        int temperatura;
+        Boolean hacesol;
+        Boolean nevando;
+        
+        //Diagrama 1
+      //  temperatura = 27;
+      //  if (temperatura > 25) {
+      //      System.out.println("A la playa!!!");
+      //  }
+        
+        //Diagrama 2
+      //  hacesol = true;
+      //  if (hacesol){
+      //      System.out.println("No te olvides la sombrilla");
+      //  }
+        
+        //Diagrama 3
+      //  hacesol = true;
+      //  nevando = true;
+      //  if (hacesol && nevando){
+      //      System.out.println("Que bien!!");
+      //  }
+        
+        //Diagrama 4
+      //  nevando = true;
+      //  temperatura = 29;
+      //  if (nevando && temperatura >= 20 && temperatura <=30){
+      //      System.out.println("No me lo creo!!");
+      //  }
+        
+      //Diagrama 5
+      //  temperatura = 32;
+      //  hacesol = true;
+      //  if (temperatura <0 || temperatura >30 && hacesol){
+      //      System.out.println("Me quedo en casa");
+      //  }  
+        
+      //Diagrama 6
+      //  temperatura = 32;
+      //  if (temperatura >25){
+      //      System.out.println("A la playa!!!");
+      //  }    
+      
+      
+      //Diagrama 7
+      //  temperatura = 10;
+      //  hacesol = true;
+      //  if (temperatura <=25){
+      //     System.out.println("Esperamos al buen tiempo");
+      //  }  
+        
+      
+      //Diagrama 8
+     //   temperatura = 10;
+     //  if (temperatura >25){
+     //       System.out.println("A la playa!!!");
+     //   } else {   
+     //      System.out.println("Esperando al buen tiempo");
+     //  }
+       
+      //Diagrama 9
+     //   temperatura = 10;
+     //   nevando = true;
+     //  if (temperatura >25){
+     //       System.out.println("A la playa!!!");
+     //   } else { 
+     //       if (temperatura <5 && nevando){
+     //      System.out.println("A esquiar!!!");
+     //       } else {
+     //        System.out.println("A Dormir!!!");   
+     //       }
+     //  } 
+       
+     //Diagrama 10
+        temperatura = 10;
+       if (temperatura <15){
+            if (temperatura <5){
+           System.out.println("A esquiar!!!");
+            } else {
+             System.out.println("A Dormir...Zzz!!!");   
+            }
+            }else {
+                if (temperatura >25){
+           System.out.println("A la playa!!!");
+            }else  {
+             System.out.println("A la montaña!!!");   
+            }
+       }        
+       }
+       }
