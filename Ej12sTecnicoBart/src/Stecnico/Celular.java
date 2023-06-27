dbfe7360e4823017eb895a8083266f1757961912
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-06-27 19:29:07 -0300

    Add files via upload

---
+++ Ej12sTecnicoBart/src/Stecnico/Celular.java
@@ -0,0 +1,35 @@
+
+package Stecnico;
+
+import java.util.Scanner;
+
+public class Celular extends Stecnico{
+    
+    Scanner leer = new Scanner(System.in);
+
+    
+public void setCelular(String Celular) {
+       
+            System.out.println("--------------------------------------------");
+            System.out.println("-----------REGISTRO del CELULAR-------------");
+            System.out.println("--------------------------------------------");
+            System.out.println("------------------D A T O S-----------------");
+            System.out.println("MARCA :" +Marca);
+            Marca=leer.nextLine();
+            System.out.println("MODELO :" + Modelo);
+            Modelo=leer.nextLine();
+            System.out.println("--------------------------------------------");
+            System.out.println("Elija el Tipo de Bateria :" + bateria);
+            System.out.println("--------------1 - 3000ma 5hs. --------------");
+            System.out.println("--------------1 - 4000ma 9hs. --------------");
+            System.out.println("--------------1 - 7000ma 12hs. --------------");
+            bateria=leer.nextLine();
+            System.out.println("--------------------------------------------");
+            System.out.println("DESPERFECTO :" + defecto);
+            defecto=leer.nextLine();
+            
+           
+    }
+     }
+         
+           
---
+++ Ej12sTecnicoBart/src/Stecnico/Joystick.java
@@ -0,0 +1,33 @@
+
+package Stecnico;
+
+import java.util.Scanner;
+
+
+public class Joystick extends Stecnico{
+  
+    Scanner leer = new Scanner(System.in);
+     
+     public void Joystick(String Joystick) {
+        this.Joystick = Joystick;
+
+            System.out.println("--------------------------------------------");
+            System.out.println("----------REGISTRO de la TABLET-------------");
+            System.out.println("--------------------------------------------");
+            System.out.println("------------------D A T O S-----------------");
+            System.out.println("MARCA :" +Marca);
+            Marca=leer.nextLine();
+            System.out.println("MODELO :" + Modelo);
+            Modelo=leer.nextLine();
+            System.out.println("--------------------------------------------");
+            System.out.println("Elija el Tipo de Bateria :" + bateria);
+            System.out.println("--------------1 - 3000ma 5hs. --------------");
+            System.out.println("--------------1 - 4000ma 9hs. --------------");
+            System.out.println("--------------1 - 7000ma 12hs. --------------");
+            bateria=leer.nextLine();
+            System.out.println("--------------------------------------------");
+            System.out.println("DESPERFECTO :" + defecto);
+            defecto=leer.nextLine();
+      }   
+    
+}     
