8cb933d6beebd5b760a281ccf0af700abb9dfaeb
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-11-07 09:34:50 -0300

    Add files via upload

---
+++ My_4Clase031123/Consulta.sql
@@ -0,0 +1,14 @@
+A- Los accesos de un cocinero 6?
+SELECT *
+FROM accesos
+WHERE idPersonal = 6;
+
+b- Los accesos del Camarero 1?
+SELECT *
+FROM accesos
+WHERE idPersonal = 1;
+
+c- El sueldo de el camarero  1 ?
+SELECT *
+FROM sueldos
+WHERE tipopersonal = 'Camarero';
