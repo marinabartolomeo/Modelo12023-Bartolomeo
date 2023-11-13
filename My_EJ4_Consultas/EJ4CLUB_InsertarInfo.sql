315046399d01dc8a0ca1cbc41dc10fd36c6cc2d8
Marina <128652157+marinabartolomeo@users.noreply.github.com>
2023-11-13 13:43:47 -0300

    Add files via upload

---
+++ My_EJ4_Consultas/EJ4CLUB_InsertarInfo.sql
@@ -0,0 +1,17 @@
+use ej4club;
+INSERT INTO deportes(`idCodDeporte`, `Deporte`, `Nombre`, `Profesor`) VALUES
+('1','Tenis','Marcelo Perez', 'Roca'),
+('2','Natacion','Marta Conde', 'Torres'),
+('3','Basquet','Luis Campo', 'Garcia'), 
+('4','Futbol','Marcelo Lopez', 'Roca'),
+('5','Voley','Jose Vidal', 'Garcia');
+
+use ej4club;
+INSERT INTO inscriptos (`idDNI`, `CodDeporte`, `Matricula`) VALUES
+('22222222', '2', 's'),
+('22222222', '4', 'n'),
+('22222222', '5', 'n');
+
+('22222222', '3', 's');
+('24444444', '3', 'n');
+('23333333', '2', 's');
